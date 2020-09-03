074a402958054940de042ee4762da7bdce29c2c9
Talha <talha_bayburtlu2@hotmail.com>
2020-09-03 20:19:30 +0300

    React redux init.

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom"
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from './App';
 import theme from "./theme/theme"
 import { ThemeProvider } from '@material-ui/core';
 
+const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose ;
+
+const rootReducer = combineReducers({
+  
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+   </React.StrictMode>  
+  </Provider>
+  ,document.getElementById('root'));
